83a534447e94c27975c80e9a5fdb28ef81c381e0
chetan <chetankharel7@gmail.com>
2017-08-24 20:43:39 +0545

    Added bower component and installed bootstrap and served it.

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace AspNetCoreStarter
 {
@@ -43,6 +45,14 @@ namespace AspNetCoreStarter
                     ExceptionHandler = ctx => ctx.Response.WriteAsync("Oops!!")
                 });
             }
+            app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                   Path.Combine(Directory.GetCurrentDirectory(), @"bower_components")),
+                   RequestPath = new PathString("/bower")
+            });
 
             app.UseMvcWithDefaultRoute();
 
