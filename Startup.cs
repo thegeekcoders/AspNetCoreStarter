256ee4bf571fd807326e4a3de99351b12f098c26
chetan <chetankharel7@gmail.com>
2017-08-24 20:10:09 +0545

    added mvc with default routes and home controller.

--- Startup.cs
+++ Startup.cs
@@ -5,16 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetCoreStarter
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddEnvironmentVariables()
+            .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+        public IConfiguration Configuration { get; set; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +36,15 @@ namespace AspNetCoreStarter
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler(new ExceptionHandlerOptions
+                {
+                    ExceptionHandler = ctx => ctx.Response.WriteAsync("Oops!!")
+                });
+            }
+
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
